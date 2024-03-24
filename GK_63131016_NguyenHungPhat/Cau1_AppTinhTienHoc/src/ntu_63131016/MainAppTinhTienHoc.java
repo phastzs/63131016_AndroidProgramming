eea57468b9d94b42ace87557613ed8d8642c7356
phastzs <phatnguyen.crypto.zs@gmail.com>
2024-03-24 14:23:06 +0700

    4.Set visible for Main App

--- GK_63131016_NguyenHungPhat/Cau1_AppTinhTienHoc/src/ntu_63131016/MainAppTinhTienHoc.java
+++ GK_63131016_NguyenHungPhat/Cau1_AppTinhTienHoc/src/ntu_63131016/MainAppTinhTienHoc.java
@@ -1,16 +1,9 @@
 package ntu_63131016;
 
-import javax.swing.SwingUtilities;
-
 public class MainAppTinhTienHoc {
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                FrameAppTinhTienHoc calculator = new FrameAppTinhTienHoc();
-                calculator.setVisible(true);
-            }
-        });
-    }
+	public static void main(String[] args) {
+		new FrameAppTinhTienHoc().setVisible(true);
+	}
+
 }
